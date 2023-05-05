2ba6b4ac457aa908567a102fabf96bf7a6b07e8d
Christian Antonsson <antonsson.christian@live.se>
2023-05-05 14:55:51 +0200

    Implementerat att skriva i ett textfält benämnt editText och att knappen save ska spara värdet för för editText till MyAppPreferenceString. Det fungerar inte att presentera texten i textView i activity_main än däremot

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences myPreferenceRef;
+    SharedPreferences.Editor myPreferenceEditor;
+    TextView prefTextRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myPreferenceRef = getSharedPreferences("MyPreferenceName", MODE_PRIVATE);
+        //myPreferenceEditor = myPreferenceRef.edit();
+        prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.textView);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "Fel text"));
+    }
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,50 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    Button backButton;
+    SharedPreferences.Editor myPreferenceEditor;
+    SharedPreferences myPreferenceRef;
+    EditText newPrefText;
+    //TextView prefTextRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        backButton = findViewById(R.id.button2);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        myPreferenceRef = getPreferences(Context.MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
     }
+    public void savePref(View v){
+        // Get the text
+        newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.editText);
+        // Store the new preference
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+        // Clear the EditText
+        newPrefText.setText("");
+    }
+
 }
